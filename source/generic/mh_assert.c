2211efeeccfb13f8c0e15cb9cdce1ed97fd8d3ca
mihail-8480 <mihail-mojsoski@outlook.com>
2021-11-28 19:36:17 +0100

    changed MH_NO_SPAM to MH_LOG_VERBOSE with the opposite effect

--- source/generic/mh_assert.c
+++ source/generic/mh_assert.c
@@ -11,8 +11,8 @@ void mh_assert_fail(String assertion, String file, count_t line, String function
 
 void mh_debug_log(String message, Any object, String file, count_t line, String function) {
 #define TRUE_STR "1"
-    var no_spam = getenv("MH_NO_SPAM");
-    if (no_spam == MH_NULL || strncmp(no_spam, TRUE_STR, sizeof TRUE_STR-1) != 0) {
+    var verbose = getenv("MH_LOG_VERBOSE");
+    if (verbose != MH_NULL && strncmp(verbose, TRUE_STR, sizeof TRUE_STR-1) == 0) {
         fprintf(stdout, MH_DEBUG_COLOR "mh_debug(0x%llx): %s:%llu: %s(): %s\n" MH_RESET_COLOR, (count_t) object, file,
                 line, function, message);
         fflush(stdout);
